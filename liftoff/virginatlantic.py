cb69e81178706a26e4df9235e7281160730b9615
Preethi266 <preethi@headrun.com>
2020-04-16 17:29:58 +0530

    #6 changed the request and fareclasses

--- liftoff/virginatlantic.py
+++ liftoff/virginatlantic.py
@@ -5,96 +5,57 @@ from pprint import pprint
 import re
 import copy
 from datetime import datetime
+from selenium import webdriver
+from pyvirtualdisplay import Display
+from settings import proxies
 
 
 def vigranAtlantic(request_data):
     data = request_data
+    display = Display(visible=0, size=(1400,1000))
+    display.start()
+    driver = webdriver.Firefox()
+    driver.get('https://www.virginatlantic.com')
+    _abck = ''.join([item.get('value') for item in driver.get_cookies() if item.get('name') == '_abck'])
+    display.stop()
+    driver.quit()
 
-    headers = {
-        'authority': 'www.virginatlantic.com',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
-        'sec-fetch-user': '?1',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    }
     departureDate = data.get('departure_date', {}).get('when', '')
-    dep_date = datetime.strptime(departureDate,"%Y-%m-%d").strftime('%m/%d/%Y')
     arrivalDate = data.get('arrival_date', {}).get('when', '')
-    try:
-        arr_date = datetime.strptime(arrivalDate,"%Y-%m-%d").strftime('%m/%d/%Y')    
-    except:
-        arr_date = ''
     tripStatus = 'ONE_WAY'
     if arrivalDate:
         tripStatus = 'ROUND_TRIP'
     else:
         arrivalDate = None
     
-    params = (
-        ('action', 'findFlights'),
-        ('tripType', tripStatus),
-        ('priceSchedule', 'PRICE'),
-        ('originCity', ''.join(data.get('departures', ''))),
-        ('destinationCity', ''.join(data.get('arrivals', ''))),
-        ('departureDate', dep_date),
-        ('departureTime', 'AT'),
-        ('returnDate', arr_date),
-        ('returnTime', ''),
-        ('searchByCabin', 'true'),
-        ('cabinFareClass', 'VSLT'),
-        ('deltaOnlySearch', 'false'),
-        ('deltaOnly', 'off'),
-        ('Go', 'Find Flights'),
-        ('meetingEventCode', ''),
-        ('refundableFlightsOnly', 'false'),
-        ('compareAirport', 'false'),
-        ('awardTravel', 'true'),
-        ('datesFlexible', 'true'),
-        ('flexAirport', 'false'),
-        ('passengerInfo', 'ADT:1'),
-    )
-    res = requests.get('https://www.virginatlantic.com/flight-search-2/search', headers=headers, params=params)
-    ak_bmsc = ''.join(re.findall('ak_bmsc=(.*)',res.headers.get('Set-Cookie',''))).split(';')[0]
-    ab_sk = ''.join(re.findall('_abck=(.*)',res.headers.get('Set-Cookie',''))).split(';')[0]
-    tltu_id = ''.join(re.findall('TLTUID=(.*)',res.headers.get('Set-Cookie',''))).split(';')[0]
-    tltu_sid = ''.join(re.findall('TLTSID=(.*)',res.headers.get('Set-Cookie',''))).split(';')[0]
-    dt_cookie = ''.join(re.findall('dtCookie=(.*)',res.headers.get('Set-Cookie',''))).split(';')[0]
-    bm_sz = ''.join(re.findall('bm_sz=(.*)',res.headers.get('Set-Cookie',''))).split(';')[0]
-
-    cookies = {
-        'TLTUID': tltu_id,
-        'TLTSID': tltu_sid,
-        'bm_sz': bm_sz,
-        'ak_bmsc': ak_bmsc,
-        #'_abck': 'C3B50BA645760D865B55BCB5BA35019C~0~YAAQyXbNFwozpRBxAQAAq1FCPwMho+7wheAe2bjfs92sasgv7qioa/Vm656W9W5ARfeiEn7daT/b0WzWKbOfqv4vksQzRQpRGBohGsEFL60L5HzC1B6CO3j67rFvsGgrUsLYl+SXfM0mRHF7SPfXYurXRJudQC+oeSWA7fbjYgqyWVE1beALTkTzj9T/xuuw0qqVljc/kUwqUcjMJKn/xh7GXU6kdRWG2Spr0pRIFvu2ggjvYrk8dF1zA0I8Qvu0eVkYtcRog22qFcAuufqovE/po+R8wi3KU33NGRbCEisslPgv44YZaYFu/EBl1do0D0zbYnPMUKjQN6Cyf17U~-1~-1~-1',
-        'dtCookie': dt_cookie,
-    }
-    
+     
     request_cabins = data.get('cabins', [])
     cabin_classes = []
-    cabinMapping = {'Economy':'economy','Premium':'premium_economy','Upper Class':'business','Upper Class':'first_class'}
+    cabinMapping = {'Economy':'Economy','Premium':'Premium Economy','Upper Class':'Business'}
     for cabin in request_cabins:
-        if cabin.lower() == "first_class":
-            cabin_classes.append("Upper Class")
-        elif cabin.lower() == "business":
+        if cabin.lower() == "business":
             cabin_classes.append("Upper Class")
         elif cabin.lower() == "premium_economy":
             cabin_classes.append("Premium")
         elif cabin.lower() == "economy":
             cabin_classes.append("Economy")
     cabin_classes = list(set(cabin_classes))
+    
     maxStop = data.get('max_stops')
+    cookies = {'_abck': _abck}
     headers = {
-        'authority': 'www.virginatlantic.com',
-        'x-app-route': 'SL-RSB',
-        'x-app-refresh': '',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
-        'content-type': 'application/json; charset=UTF-8',
-        'x-dtreferer': 'https://www.virginatlantic.com/flight-search-2/flexible-dates?cacheKeySuffix=92348bf5-1594-4388-8e13-66ae2d083a0e',
-        'cachekey': '92348bf5-1594-4388-8e13-66ae2d083a0e',
-        'origin': 'https://www.virginatlantic.com',
-        'referer': 'https://www.virginatlantic.com/flight-search-2/search-results?cacheKeySuffix=92348bf5-1594-4388-8e13-66ae2d083a0e',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'authority': 'www.delta.com',
+            'pragma': 'no-cache',
+            'cache-control': 'no-cache',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36',
+            'content-type': 'application/json; charset=UTF-8',
+            'accept': 'application/json',
+            'origin': 'https://www.delta.com',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            'accept-language': 'en-US,en;q=0.9,fil;q=0.8,te;q=0.7',
+
     }
 
     data = {"bestFare":"VSLT",
@@ -125,13 +86,12 @@ def vigranAtlantic(request_data):
             "sortableOptionId":"priceAward",
             "requestPageNum":"1"
             }
-    response = requests.post('https://www.virginatlantic.com/shop/ow/search', headers=headers, cookies=cookies, data=json.dumps(data))
+    response = requests.post('https://www.virginatlantic.com/shop/ow/search', headers=headers, cookies=cookies, data=json.dumps(data),proxies= proxies)
     if response.status_code == 403:
-        response = requests.post('https://www.virginatlantic.com/shop/ow/search', headers=headers, cookies=cookies, data=json.dumps(data)) 
+        response = requests.post('https://www.virginatlantic.com/shop/ow/search', headers=headers, cookies=cookies, data=json.dumps(data),proxies=proxies) 
     
     final_dict = []
-    data = json.loads(response.text)
-    
+    data = json.loads(response.text)    
     error_msg = data.get('shoppingError',{}).get('error',{}).get('message',{}).get('message','')
     if error_msg:
         print(error_msg)
@@ -228,13 +188,11 @@ def vigranAtlantic(request_data):
                 for element in final_sub_dict["connections"]:
                     nameOfCabin = element["cabin"]
                     result_cabin = any(ele in nameOfCabin for ele in cabin_classes)
+                    result_cabin_name = ''.join([ele for ele in cabin_classes if(ele.lower() in nameOfCabin.lower())])
                     if result_cabin:
+                        element["cabin"] = cabinMapping[result_cabin_name]
                         cabin_available = True
                 if cabin_available:
                     final_dict.append(final_sub_dict)
 
-    return final_dict, error_msg
-
-        
-            
-            
+    return final_dict, error_msg         
