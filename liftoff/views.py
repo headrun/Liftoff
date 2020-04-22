1b85c7a368df5f2e5472c7502ab7be5e7371c501
malla794 <charan@headrun.com>
2020-04-22 10:30:13 +0530

    #4 adding united mileage api changes

--- liftoff/views.py
+++ liftoff/views.py
@@ -7,10 +7,12 @@ from .americanAirline import americanAirlines
 from .aeroplan import aeroplanselnium
 from .virginatlantic import vigranAtlantic
 from .deltaMiles import DeltaSkyMiles
+from .united_mileage import UnitedMileagePlus
 
 SCRAPERS_DICT = {
     "AA": [americanAirlines, "https://www.aa.com"], "AC": [aeroplanselnium, "https://www.aeroplan.com"],
-    "VS": [vigranAtlantic, "https://www.virginatlantic.com"], "DL":[DeltaSkyMiles, "https://www.delta.com"]
+    "VS": [vigranAtlantic, "https://www.virginatlantic.com"], "DL":[DeltaSkyMiles, "https://www.delta.com"],
+    "UA": [UnitedMileagePlus, "https://www.united.com"]
     }
 
 @api_view(['POST'])
