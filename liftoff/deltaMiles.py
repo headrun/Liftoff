a8de5399a0717d461b5af48f06385d78ae01f266
Preethi266 <preethi@headrun.com>
2020-07-08 16:26:08 +0530

    #949 flighttime and layovertime issues and cabin mappping

--- liftoff/deltaMiles.py
+++ liftoff/deltaMiles.py
@@ -18,19 +18,24 @@ def DeltaSkyMiles(request_data):
     driver.quit()
      
     cabin_classes = []
-    cookies = {'_abck': _abck}
+    cookies = {'_abck':_abck}
     headers = {
         'authority': 'www.delta.com',
         'pragma': 'no-cache',
         'cache-control': 'no-cache',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36',
+        'x-app-route': 'SL-RSB',
+        'x-app-refresh': '',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
         'content-type': 'application/json; charset=UTF-8',
         'accept': 'application/json',
+        'x-app-channel': 'sl-sho',
+        'cachekey': 'ac8f3c85-3418-4a10-803f-fa21499d7a15',
         'origin': 'https://www.delta.com',
         'sec-fetch-site': 'same-origin',
         'sec-fetch-mode': 'cors',
-        'sec-fetch-dest': 'empty',
-        'accept-language': 'en-US,en;q=0.9,fil;q=0.8,te;q=0.7',
+        'referer': 'https://www.delta.com/flight-search/search-results?cacheKeySuffix=ac8f3c85-3418-4a10-803f-fa21499d7a15',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
         }
 
     cabinMapping = {'Economy':'Economy',
@@ -56,11 +61,11 @@ def DeltaSkyMiles(request_data):
     cabin_hierarchy = []
     request_cabins = request_data.get('cabins', [])
     for cabin in request_cabins:
-        if cabin.lower() == "first_class":
+        if cabin.lower() == "first":
             cabin_classes.append("First")
             cabin_classes.append("Delta One")
             cabin_classes.append("Premier")
-            cabin_hierarchy = cabin_hierarchy + ["First Class", "Premium Economy", "Business", "Economy", "Main Cabin", "Delta One", "Classic", "Basic", "First", "Upper Class", "Premier"]
+            cabin_hierarchy = cabin_hierarchy + ["First Class", "Premium Economy", "Business", "Economy", "Main Cabin", "Delta One", "Classic", "Basic", "First", "Upper Class", "Premier","Comfort+"]
         elif cabin.lower() == "business":
             cabin_classes.append("Business")
             cabin_classes.append("Upper Class")
@@ -83,6 +88,7 @@ def DeltaSkyMiles(request_data):
     maxStop = request_data.get('max_stops')
     req_page = 0
     final_dict = []
+    error_msg = ''
     while req_page >= 0:
         req_page = req_page + 1
         data = {"bestFare":"BE",
@@ -106,24 +112,27 @@ def DeltaSkyMiles(request_data):
                 "upgradeRequest":False,
                 "corporateSMTravelType":"Personal",
                 "pageName":"FLIGHT_SEARCH",
-                "cacheKey":"b80b4b85-198c-4bfb-9c34-7da421b302d5",
+                "cacheKey":"ac8f3c85-3418-4a10-803f-fa21499d7a15",
                 "actionType":"",
                 "initialSearchBy":{"fareFamily":"BE", "meetingEventCode":"", "refundable":False, "flexAirport":False, "flexDate":True, "flexDaysWeeks":"FLEX_DAYS"},
                 "sortableOptionId":"priceAward",
                 #"requestPageNum":"2"}
                 "requestPageNum":str(req_page)}
-        response = requests.post('https://www.delta.com/shop/ow/search', headers=headers, cookies=cookies, data=json.dumps(data), proxies=proxies) 
+        response = requests.post('https://www.delta.com/shop/ow/search', headers=headers, cookies=cookies, data=json.dumps(data), proxies=proxies)
         if response.status_code == 400:
-            break
-        else:
-            if response.status_code == 200:
+            try:
                 data = json.loads(response.text)
-                error_msg = data.get('shoppingError', {}).get('error', {}).get('message', {}).get('code', '')
-                if error_msg:
-                    break
+                error_msg = data.get('shoppingError', {}).get('error', {}).get('message', {}).get('message', '')
+            except:data, error_msg = '', ''
+            if error_msg:
+                error_msg = 'invalid airlines'
+                return final_dict, error_msg
+            else:
+                break
         data = json.loads(response.text)
         error_msg = data.get('shoppingError', {}).get('error', {}).get('message', {}).get('message', '')
         if error_msg:
+            error_msg = 'No flights found'
             print(error_msg)
             return final_dict, error_msg
 
@@ -144,13 +153,13 @@ def DeltaSkyMiles(request_data):
                     sample_dict["connections"] = []
                     fare_classes = {}
                     segments = trip.get('flightSegment', [])
-                    time = []
+                    time, tot_time, lay_hours, lay_time = [], [], [], []
                     for segment in segments:
                         legs = segment.get('flightLeg', [])
                         for leg in legs:
                             total_time = 0
-                            hours = 0
-                            minuts = 0
+                            lay_hour, lay_minut = 0, 0
+                            hours, minuts = 0, 0
                             airlineDetails = {}
                             departureAirport = leg.get('originAirportCode', '')
                             arrivalAirport = leg.get('destAirportCode', '')
@@ -175,43 +184,51 @@ def DeltaSkyMiles(request_data):
                             flighttimes = str(leg.get('duration', {}).get('hour', '')) + ':' + str(leg.get('duration', {}).get('minute', ''))
                             hours = hours+leg.get('duration', {}).get('hour', '')
                             minuts = minuts+leg.get('duration', {}).get('minute', '')
-                            time.append(hours)
+                            tot_time.append(hours)
                             time.append(minuts)
-                            minit = None
-                            try:
-                                total_time = str(time[0]+time[2]) + ':'+str(time[1]+time[3])
-                                minite = time[1]+time[3]
-                                if minite > 60:
-                                    hour = 1
-                                    minite1 = minite - 60
-                                    minit = str(time[0]+time[2] + hour) + ':'+ str(minite1)
-                                else:
-                                    minit = str(time[0]+time[2]) + ':'+str(time[1]+time[3])
-                            except:
-                                minit = str(time[0])+':'+str(time[1])
+                            minit = str(sum(tot_time))+":"+str(sum(time))
+                            minite = sum(time)
+                            if minite >= 60:
+                                hour = 1
+                                minite1 = minite - 60
+                                minit = str(sum(tot_time)+hour)+":"+str(minite1)
+                            else:
+                                minit = str(sum(tot_time))+":"+str(sum(time))
+                            flight_time = datetime.strptime(minit, '%H:%M').strftime('%H:%M')
                             airlineDetails["redemptions"] = None
                             airlineDetails["payments"] = None
                             airlineDetails["tickets"] = None
-                            sample_dict["connections"].append(airlineDetails)
-     
-                            layover = sample_dict["connections"][0].get('times', {}).get('layover', '')
-                            if layover == '':
-                                layover = None
-                            sample_dict['times'] = {'flight':minit, 'layover': layover}
                         
-                        totalAirTime = str(segment.get('totalAirTime', {}).get('hour', '')) + ':' + str(segment.get('totalAirTime', {}).get('minute', ''))
-                        try:
-                            totalAirTimes = datetime.strptime(totalAirTime, '%H:%M').strftime('%H:%M')
-                        except:
-                            totalAirTimes = None
-                        layoverTime = str(segment.get('layover', {}).get('duration', {}).get('hour', '')) + ':'+ str(segment.get('layover', {}).get('duration', {}).get('minute', ''))
-                        try:
-                            layoverTimes = datetime.strptime(layoverTime, '%H:%M').strftime('%H:%M')
-                        except:
-                            layoverTimes = None
-                        airlineDetails["times"] = {'flight': totalAirTimes, 'layover': layoverTimes}
-                        #sample_dict["site_key"] = 'DL'    
-                        sample_dict["site_key"] = request_data['site_key']
+                            totalAirTime = str(segment.get('totalAirTime', {}).get('hour', '')) + ':' + str(segment.get('totalAirTime', {}).get('minute', ''))
+                            try:
+                                totalAirTimes = datetime.strptime(totalAirTime, '%H:%M').strftime('%H:%M')
+                            except:
+                                totalAirTimes = None
+                            layoverTime = str(segment.get('layover', {}).get('duration', {}).get('hour', '')) + ':'+ str(segment.get('layover', {}).get('duration', {}).get('minute', ''))
+                            try:
+                                layoverTimes = datetime.strptime(layoverTime, '%H:%M').strftime('%H:%M')
+                            except:
+                                layoverTimes = None
+                            airlineDetails["times"] = {'flight': totalAirTimes, 'layover': layoverTimes}
+                            sample_dict["connections"].append(airlineDetails)
+                            lay_hour = lay_hour+segment.get('layover', {}).get('duration', {}).get('hour', 0)
+                            lay_minut = lay_minut+segment.get('layover', {}).get('duration', {}).get('minute', 0)
+                            lay_hours.append(lay_hour)
+                            lay_time.append(lay_minut)
+                            lay_minite = str(sum(lay_hours))+":"+str(sum(lay_time))
+                            lay_minute = sum(lay_time)
+                            if lay_minute >= 60:
+                                hour = 1
+                                lay_minite1 = minite - 60
+                                lay_minute = str(sum(lay_hours)+hour)+":"+str(lay_minite1)
+                            else:
+                                lay_minite = str(sum(lay_hours))+":"+str(sum(lay_time))
+                            layover_time = datetime.strptime(lay_minite,'%H:%M').strftime('%H:%M')
+                            if layover_time == '00:00':
+                                layover_time = None
+                            sample_dict['times'] = {'flight':flight_time, 'layover':layover_time}
+                            #sample_dict["site_key"] = 'DL'    
+                            sample_dict["site_key"] = request_data['site_key']
 
             if not Flag:
                 continue
@@ -231,8 +248,6 @@ def DeltaSkyMiles(request_data):
                 award = []
                 for name in cabinname:
                     cabinName = name.get('brandName', '')
-                    if cabinName == 'Main':
-                        cabinName = 'Economy'
                     if '&#' in cabinName:
                         cabinName = ''.join(re.findall('(.*)&#', cabinName))
                     final_sub_dict["connections"][index]["cabin"] = cabinName
@@ -254,7 +269,7 @@ def DeltaSkyMiles(request_data):
                 if len(cabinname) != 0:
                     cabin_available = False
                     for element in final_sub_dict["connections"]:
-                        nameOfCabin = element["cabin"]                            
+                        nameOfCabin = element["cabin"]
                         result_cabin = any(ele in nameOfCabin for ele in cabin_classes)
                         result_cabin_name = ''.join([ele for ele in cabin_classes if(ele.lower() in nameOfCabin.lower())])
                         if result_cabin_name == 'EconomyClassic' or result_cabin_name == 'ClassicEconomy':
@@ -262,6 +277,8 @@ def DeltaSkyMiles(request_data):
                         if result_cabin:
                             element["cabin"] = cabinMapping[result_cabin_name]
                             cabin_available = True
+                        elif nameOfCabin == 'Main':
+                            element["cabin"] = 'Economy'
                  
                     if cabin_available:
                         hierarchy_valid = True
@@ -272,3 +289,5 @@ def DeltaSkyMiles(request_data):
                         if hierarchy_valid:
                             final_dict.append(final_sub_dict)
     return final_dict, error_msg
+
+
